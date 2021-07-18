b5ef00caf79fd6356c7d7c550bb216bb215722a6
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-07-18 22:54:07 +0300

    changed Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 public class Main {
 
     private final static int PRODUCT_COUNT = 100;
+    private final static String CSV_DIVIDER = ",";
+    private final static String CSV_PATH = "./csv/products-data.csv";
     private final static String URL = "https://gpsfront.aliexpress.com/getRecommendingResults.do?widget_id=5547572&postback=acfff392-5ed4-4a90-a3a9-cd2b552630dd";
-    private final static String CSV_PATH = "./csv/";
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         DataStorage dataStorage = new Grabber(URL).parse(PRODUCT_COUNT);
-        CSVCreator.saveData(dataStorage, CSV_PATH);
+        CSVCreator.saveData(dataStorage, CSV_PATH, CSV_DIVIDER);
     }
 }
